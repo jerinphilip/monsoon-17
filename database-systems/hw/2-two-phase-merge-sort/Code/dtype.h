8f60c00f94155ba8a460078dfee8a1158e33fe5d
Jerin Philip <jerin.philip@students.iiit.ac.in>
2017-09-07 18:38:51 +0530

    writeout, moving to k-way merge

--- database-systems/hw/2-two-phase-merge-sort/Code/dtype.h
+++ database-systems/hw/2-two-phase-merge-sort/Code/dtype.h
@@ -4,6 +4,41 @@ using namespace std;
 
 typedef vector<string> row;
 
+struct info {
+    map<string, int> index;
+    vector<pair<int, int>> sizes;
+    int record_length;
+    int expected_size;
+
+
+   info(string filename){
+        int size, offset, count, pos;
+        string column, line, size_str;
+        char delimiter;
+        fstream meta;
+
+        meta.open(filename.c_str(), ios::in);
+        record_length = 0;
+        offset = 0;
+        count = 0;
+        delimiter = ',';
+        while (getline(meta, line)){
+            pos = line.find(delimiter);
+            column = line.substr(0, pos);
+            size_str = line.substr(pos+1, string::npos);
+            size = atoi(size_str.c_str());
+            sizes.push_back(make_pair(offset, size));
+            offset += size + 2 /* Space character */;
+            record_length += size;
+            index[column] = count;
+            count += 1;
+        }
+        expected_size = offset-1;
+        meta.close();
+    }
+
+};
+
 class table {
     vector<row> *T;
 
@@ -25,11 +60,13 @@ class table {
     friend ostream & operator << (ostream &out, table &t){
         for(auto &row: *(t.T)){
             for(auto &datum: row){
-                out << datum << "<>";
+                out << datum << "  ";
             }
             out << "\n";
         }
         return out;
     }
+
 };
+
 
